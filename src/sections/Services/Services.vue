29e3773184d4a1d1b3f18115167e2213bde98bcf
velomad <48247308+velomad@users.noreply.github.com>
2021-01-03 04:57:08 +0530

    sections added

---
+++ src/sections/Services/Services.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid grid-cols-4 gap-10">
+    <div v-for="(item, index) in test" :key="index">
+      <Card
+        heading="PERFORMANCE PROGRAMMATIC WAY"
+        content="With our breakthrough technology we team historical user data and artificial intelligence that achieve performance goals."
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "../../components/Card";
+export default {
+  name: "Services",
+  components: { Card },
+
+  data() {
+    return {
+      test: [1, 2, 3, 4, 5, 6, 7, 8],
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="home">
+  <div>
     <HomeScreen />
+    <ServicesScreen />
+    <ContactUs />
   </div>
 </template>
 
 <script>
 import HomeScreen from "../sections/HomeScreen/Homescreen";
+import ServicesScreen from "../sections/Services/Services";
+import ContactUs from "../sections/ContactUs/ContactUs";
 
 export default {
   name: "Home",
-  components:{
-    HomeScreen
-  }
+  components: {
+    HomeScreen,
+    ServicesScreen,
+    ContactUs,
+  },
 };
 </script>
