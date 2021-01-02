3488dfe0d483b31835383e295318e6793ce52bc0
A00079 <bajay0007@gmail.com>
2021-01-03 02:30:42 +0530

    Basic SetUp

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="nav">
-  </div>
   <router-view />
 </template>
-
+<script>
+export default {
+  name: "app"
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -11,18 +13,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
