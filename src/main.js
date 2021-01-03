20238f3131e89223c7447640d452f618a9e0dfa9
A00079 <bajay0007@gmail.com>
2021-01-03 18:22:06 +0530

    Bug

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+/* eslint-disable */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueScrollTo from "vue-scrollto";
+const Vue = require('vue')
+Vue.use(VueScrollTo)
 
 createApp(App)
   .use(router)
